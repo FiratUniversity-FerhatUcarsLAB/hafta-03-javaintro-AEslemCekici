8cca695b1f255b5116c8f19c5d66b81adca398f3
AEslemCekici <250541026@firat.edu.tr>
2025-10-22 22:57:14 +0300

    Update DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,12 @@
+public class DikdörtgenHesap {
+    public static void main(String[] args) {
+        double en=4.5;
+        double boy=7.9;
+
+        double alan=en*boy;
+        double çevre=2*(en+boy);
+
+        System.out.println("alan="+alan);
+        System.out.println("çevre="+çevre);
+    }
+}
