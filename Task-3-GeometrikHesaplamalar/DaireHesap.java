e6c38588557630d9659cdb54b9799a74721f4a88
AEslemCekici <250541026@firat.edu.tr>
2025-10-22 22:46:08 +0300

    Update DaireHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -0,0 +1,11 @@
+public class DaireHesap {
+    public static void main(String[] args) {
+    double r=5.5;
+    double pi=3.14159;
+
+    double alan=pi*r*r;
+    double çevre=2*pi*r;
+    System.out.println("alan="+alan);
+    System.out.println("çevre="+çevre);
+    }
+}
