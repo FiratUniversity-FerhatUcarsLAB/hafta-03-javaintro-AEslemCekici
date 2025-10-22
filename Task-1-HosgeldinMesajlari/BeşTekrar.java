26e604ce5d37a0171a327d4aa2317ea4fb99a9cf
AEslemCekici <250541026@firat.edu.tr>
2025-10-22 22:20:35 +0300

    Add files via upload

---
+++ Task-1-HosgeldinMesajlari/BeşTekrar.java
@@ -0,0 +1,11 @@
+public class BeşTekrar {
+    public static void main(String[] args) {
+        System.out.println("Java`ya Hoş Geldin");
+        System.out.println("Java`ya Hoş Geldin");
+        System.out.println("Java`ya Hoş Geldin");
+        System.out.println("Java`ya Hoş Geldin");
+        System.out.println("Java`ya Hoş Geldin");
+
+    }
+
+}
