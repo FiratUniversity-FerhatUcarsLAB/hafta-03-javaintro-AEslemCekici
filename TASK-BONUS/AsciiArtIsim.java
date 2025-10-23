ca079e0cdcdde2cc3f3a698fb4b7af43f9a69fa2
AEslemCekici <250541026@firat.edu.tr>
2025-10-23 20:29:02 +0300

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,19 @@
+public class AsciiArtİsim {
+    public static void main(String[] args) {
+        System.out.println("   *      *    *  *****    *****");
+        System.out.println("  * *      *  *   *        * ");
+        System.out.println(" *****      *     *****    *****");
+        System.out.println("*     *     *         *    * ");
+        System.out.println("*      *    *     *****    *****");
+
+        System.out.println(    );
+
+
+        System.out.println("***** *****  *       *****  *       *");
+        System.out.println("*     *      *       *      * *   * *");
+        System.out.println("***** *****  *       *****  *   * *  *");
+        System.out.println("*         *  *       *      *    *   *");
+        System.out.println("***** *****  ******  *****  *        *");
+
+    }
+}
