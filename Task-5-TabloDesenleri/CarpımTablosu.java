bee12bf434d6dcd458e28cc8d5995968e2f04790
AEslemCekici <250541026@firat.edu.tr>
2025-10-23 19:57:30 +0300

    Add files via upload

---
+++ Task-5-TabloDesenleri/CarpımTablosu.java
@@ -0,0 +1,21 @@
+public class CarpımTablosu {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== İkiler Çarpım Tablosu ===");
+        System.out.println("-----******-----");
+
+        System.out.println("2 x 1 = 2");
+        System.out.println("2 x 2 = 4");
+        System.out.println("2 x 3 = 6");
+        System.out.println("2 x 4 = 8");
+        System.out.println("2 x 5 = 10");
+        System.out.println("2 x 6 = 12");
+        System.out.println("2 x 7 = 14");
+        System.out.println("2 x 8 = 16");
+        System.out.println("2 x 9 = 18");
+        System.out.println("2 x 10 = 20");
+
+        System.out.println("-----*****-----");
+    }
+}
