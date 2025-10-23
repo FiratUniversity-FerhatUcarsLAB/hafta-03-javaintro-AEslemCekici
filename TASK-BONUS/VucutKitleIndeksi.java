f6aeb064f7158e0008dde53130986b78141a9db8
AEslemCekici <250541026@firat.edu.tr>
2025-10-23 21:46:56 +0300

    Update VucutKitleIndeksi.java

---
+++ TASK-BONUS/VucutKitleIndeksi.java
@@ -0,0 +1,12 @@
+public class VücutKutleIndeksi {
+    public static void main(String[] args) {
+      double kilo=53;
+      double boy=1.60;
+      double BMI= kilo / (boy * boy);
+      
+      System.out.println("BMI: "+BMI);
+      System.out.println("Kilo: "+kilo);
+      System.out.println("Boy: "+boy);
+
+    }
+}
