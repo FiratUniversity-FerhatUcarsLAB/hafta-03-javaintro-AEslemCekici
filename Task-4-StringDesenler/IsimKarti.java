e2a814849f1f3206fea669c18b072bf32bedc6d4
AEslemCekici <250541026@firat.edu.tr>
2025-10-23 11:46:34 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,19 @@
+public class IsimKarti {
+
+    public static void main(String[] args) {
+        String Ad="Ayşe Eslem";
+        String Soyad="ÇEKİCİ";
+        String oğrenciNo="250541026";
+        String Bolum = "Yazılım Mühendisligi";
+        System.out.println("*===*===*===*===*===*===*");
+        System.out.println(" OGRENCI BILGILERI ");
+        System.out.println("*===*===*===*===*===*===*");
+        System.out.println("Ad: " + Ad);
+        System.out.println("Soyad : " + Soyad);
+        System.out.println("No : " + oğrenciNo);
+        System.out.println("Bolum : " + Bolum);
+        System.out.println("*===*====*====*====*===*");
+
+    }
+
+}
